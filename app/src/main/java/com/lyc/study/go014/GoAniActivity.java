d08133d666c1d0e7fb4c92d576990def2818fced
lyc <liuyicheng@etouch.cn>
2017-05-16 00:06:00 +0800

    动画

--- app/src/main/java/com/lyc/study/go014/GoAniActivity.java
+++ app/src/main/java/com/lyc/study/go014/GoAniActivity.java
@@ -1,9 +1,20 @@
 package com.lyc.study.go014;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
 
 import com.lyc.study.R;
 
@@ -12,14 +23,75 @@ import com.lyc.study.R;
  */
 
 public class GoAniActivity extends Activity {
+    private Button btn_sleep_go,btn_mult,btn_keyframe;
+    private View iv_target_01;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ani);
-
-
         initGift();
+        initViews();
+    }
+
+    private void initViews() {
+        btn_sleep_go= (Button) findViewById(R.id.btn_sleep_go);
+
+        btn_sleep_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ObjectAnimator  ani_y= ObjectAnimator.ofFloat(iv_target_01,"translationY",0,50);
+                ObjectAnimator  ani_x= ObjectAnimator.ofFloat(iv_target_01,"translationX",0,100);
+                ani_y.setDuration(1000);
+                ani_x.setDuration(2000);
+                ValueAnimator pause =ValueAnimator.ofInt(0,100);
+                //设置一个空白animator  专门用于停止一段时间
+                pause.setDuration(3000);
+                AnimatorSet set = new AnimatorSet();
+                set.playSequentially(ani_y,pause,ani_x);
+                set.setDuration(6000);
+                set.start();
+            }
+        });
+
+        btn_mult= (Button) findViewById(R.id.btn_mult);
+        btn_mult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PropertyValuesHolder vh_y=PropertyValuesHolder.ofFloat("translationY",0f,50f);
+                PropertyValuesHolder vh_x=PropertyValuesHolder.ofFloat("translationX",0f,50f);
+                PropertyValuesHolder vh_alpha = PropertyValuesHolder.ofFloat("alpha", 1.0f,0f, 1.0F);
+                ObjectAnimator animator =ObjectAnimator.ofPropertyValuesHolder(iv_target_01,vh_y,vh_x,vh_alpha);
+                animator.setDuration(2000);
+
+                animator.start();
+            }
+        });
+
+
+        btn_keyframe= (Button) findViewById(R.id.btn_keyframe);
+        btn_keyframe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Keyframe keyframe = Keyframe.ofFloat(0, 0);
+                keyframe.setInterpolator(new LinearInterpolator());
+                Keyframe keyframe1 = keyframe.ofFloat(0.1f, 30);
+                keyframe1.setInterpolator(new LinearInterpolator());
+                //两个设置为相同的值就会在这里停一下  停大约  0.8*总时间
+                Keyframe keyframe2 = keyframe.ofFloat(0.9f, 30);
+                keyframe2.setInterpolator(new LinearInterpolator());
+
+                Keyframe keyframe5 = keyframe.ofFloat(1, 200);
+                keyframe5.setInterpolator(new LinearInterpolator());
+
+                PropertyValuesHolder propertyValuesHolder = PropertyValuesHolder.ofKeyframe("translationX", keyframe,
+                                keyframe1,keyframe2,keyframe5);
+                ObjectAnimator.ofPropertyValuesHolder(iv_target_01,
+                        propertyValuesHolder).setDuration(3000).start();
+            }
+        });
+
+        iv_target_01= findViewById(R.id.iv_target_01);
     }
 
     private void initGift(){
